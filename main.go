aff6f38fab2727f236b578d42616346e6456b358
johnkamauwamunga <johnkamauwamunga@gmail.com>
2026-01-13 02:12:01 +0300

    go summation

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	fmt.Println("hello john")
+	fmt.Println("hello john, this is the innitial setup for a fiber app")
+
+	a := 3
+	b := 4
+
+	fmt.Println("sum is:", a+b)
 	app := fiber.New()
 
 	log.Fatal(app.Listen(":4000"))
